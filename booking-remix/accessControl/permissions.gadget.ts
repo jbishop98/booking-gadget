d3a810bc4acd72726f92437c95249ecaf4daca62
jbishop98 <thebishman@gmail.com>
2025-02-07 22:38:39 +1300

    Add folder to repository

---
+++ booking-remix/accessControl/permissions.gadget.ts
@@ -0,0 +1,65 @@
+import type { GadgetPermissions } from "gadget-server";
+
+/**
+ * This metadata describes the access control configuration available in your application.
+ * Grants that are not defined here are set to false by default.
+ *
+ * View and edit your roles and permissions in the Gadget editor at https://booking-remix.gadget.app/edit/settings/permissions
+ */
+export const permissions: GadgetPermissions = {
+  type: "gadget/permissions/v1",
+  roles: {
+    "shopify-app-users": {
+      storageKey: "Role-Shopify-App",
+      models: {
+        shopifyFulfillment: {
+          read: {
+            filter:
+              "accessControl/filters/shopify/shopifyFulfillment.gelly",
+          },
+        },
+        shopifyGdprRequest: {
+          read: {
+            filter:
+              "accessControl/filters/shopify/shopifyGdprRequest.gelly",
+          },
+          actions: {
+            create: true,
+            update: true,
+          },
+        },
+        shopifyOrder: {
+          read: {
+            filter:
+              "accessControl/filters/shopify/shopifyOrder.gelly",
+          },
+        },
+        shopifyShop: {
+          read: {
+            filter: "accessControl/filters/shopify/shopifyShop.gelly",
+          },
+          actions: {
+            install: true,
+            reinstall: true,
+            uninstall: true,
+            update: true,
+          },
+        },
+        shopifySync: {
+          read: {
+            filter: "accessControl/filters/shopify/shopifySync.gelly",
+          },
+          actions: {
+            abort: true,
+            complete: true,
+            error: true,
+            run: true,
+          },
+        },
+      },
+    },
+    unauthenticated: {
+      storageKey: "unauthenticated",
+    },
+  },
+};
